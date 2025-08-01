abe7af1cdb6473adad24f87c2ebb3b59c6d75620
Isaiah Sherman <shermancg@gmail.com>
2025-08-01 11:06:34 -0700

    Blend tree, running anim, player look rotation

--- Assets/Scripts/Player/PlayerStateMachine.cs
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerStateMachine : StateMachine
 {
     [field: SerializeField] public InputReader InputReader { get; private set; }
+    [field: SerializeField] public CharacterController CharacterController { get; private set; }
+    [field: SerializeField] public Animator Animator { get; private set; }
+    [field: SerializeField] public float FreeLookMovementSpeed  { get; private set; }
 
     void Awake()
     {
--- Assets/Scripts/Player/PlayerTestState.cs
+++ Assets/Scripts/Player/PlayerTestState.cs
@@ -15,8 +15,18 @@ public class PlayerTestState : PlayerBaseState
         Vector3 movement = new Vector3();
         movement.x = stateMachine.InputReader.MovementValue.x;
         movement.y = 0f;
-        movement.z = stateMachine.InputReader.MovementValue.y;  
-        stateMachine.transform.Translate(movement * deltaTime);
+        movement.z = stateMachine.InputReader.MovementValue.y;
+        stateMachine.CharacterController.Move(movement * stateMachine.FreeLookMovementSpeed * deltaTime);
+
+        if (stateMachine.InputReader.MovementValue == Vector2.zero)
+        {
+            stateMachine.Animator.SetFloat("FreeLookSpeed", 0f, 0.1f, deltaTime);
+            return;
+        }
+        
+        stateMachine.Animator.SetFloat("FreeLookSpeed", 1f, 0.1f, deltaTime);
+        stateMachine.transform.rotation = Quaternion.LookRotation(movement);
+
     }
 
     public override void Exit()
